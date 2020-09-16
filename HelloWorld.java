7f516952a5086dab19d3e645b447654477412917
sarthakaggarwal97 <sarthakaggarwal97@gmail.com>
2020-09-16 11:50:53 +0000

    Checking build #3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Contribution from bubble ");
-for (int i = 0; i < 10; i++) { 
-			System.out.println("Hello World"); 
+for (int i = 0; i < 5; i++) { 
+			System.out.println("Citi is making us learn jenkins"); 
 		} 		
 System.out.println("Hello, Sarthak");	
 	}
